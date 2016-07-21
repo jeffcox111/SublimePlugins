3c2bd86cd6852de02bb874306ba7cf20b0360618
Kevin Fisher <megabytefisher@gmail.com>
2016-07-20 19:36:25 -0500

    simple handling of 'IIf' statements

--- convertsql/convertsql.py
+++ convertsql/convertsql.py
@@ -13,11 +13,15 @@ class convertsqlCommand(sublime_plugin.TextCommand):
 		content = content.replace('dbo_', 'dbo.')
 
 		# Replace trim with rtrim + ltrim
-		#content = content.replace('Trim(', 'rtrim(ltrim(')
 		content = re.sub('trim\(([^\)]*)\)', self.createTrim, content);
 
+		# Replace IIf with CASE WHEN
+		content = re.sub('iif\w*\(([^,]*),([^,]*),([^\)]*)\)', self.createCaseWhen)
 
 		self.view.replace(edit, region, content)
 
 	def createTrim(sender, matchobj):
-			return 'rtrim(ltrim(' + matchobj.group(1) + '))'
+		return 'rtrim(ltrim(' + matchobj.group(1) + '))'
+
+	def createCaseWhen(sender, matchobj):
+		return 'CASE WHEN ' + matchobj.group(1) + ' THEN ' + matchobj.group(2) + ' ELSE ' + matchobj.group(3)

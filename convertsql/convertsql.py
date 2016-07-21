07ef444df561cc7012d5e8b405b6fd99159ca92e
Kevin Fisher <megabytefisher@gmail.com>
2016-07-20 19:07:16 -0500

    Proper handling of 'trim'

--- convertsql/convertsql.py
+++ convertsql/convertsql.py
@@ -1,6 +1,7 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, re
 
 class convertsqlCommand(sublime_plugin.TextCommand):
+
 	def run(self, edit):
 		region = sublime.Region(0, self.view.size())
 		content = self.view.substr(region)
@@ -10,6 +11,13 @@ class convertsqlCommand(sublime_plugin.TextCommand):
 		content = content.replace('[', '')
 		content = content.replace(']', '')
 		content = content.replace('dbo_', 'dbo.')
-		content = content.replace('Trim(', 'rtrim(ltrim(')
+
+		# Replace trim with rtrim + ltrim
+		#content = content.replace('Trim(', 'rtrim(ltrim(')
+		content = re.sub('trim\(([^\)]*)\)', self.createTrim, content);
+
+
 		self.view.replace(edit, region, content)
-		
+
+	def createTrim(sender, matchobj):
+			return 'rtrim(ltrim(' + matchobj.group(1) + '))'

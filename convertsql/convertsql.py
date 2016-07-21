04597d7d1547848e8f59b1e301215d0637fc178e
Kevin Fisher <megabytefisher@gmail.com>
2016-07-20 19:51:06 -0500

    Can handle nested ifs and such
    also fixed dumb regex mistake

--- convertsql/convertsql.py
+++ convertsql/convertsql.py
@@ -1,6 +1,7 @@
 import sublime, sublime_plugin, re
 
 class convertsqlCommand(sublime_plugin.TextCommand):
+	runConvertAgain = false
 
 	def run(self, edit):
 		region = sublime.Region(0, self.view.size())
@@ -13,18 +14,18 @@ class convertsqlCommand(sublime_plugin.TextCommand):
 		content = content.replace('dbo_', 'dbo.')
 
 		# Replace trim with rtrim + ltrim
-		content = re.sub('\wtrim\(([^\)]*)\)', self.createTrim, content);
+		content = re.sub('\Wtrim\(([^\)]*)\)', self.createTrim, content);
 
 		# Replace IIf with CASE WHEN
-		content = re.sub('\wiif\w*\(([^,]*),([^,]*),([^\)]*)\)', self.createCaseWhen, content)
+		content = re.sub('\Wiif\w*\(([^,]*),([^,]*),([^\)]*)\)', self.createCaseWhen, content)
 
 		# Replace DATE() with GETDATE()
-		content = re.sub('\wDATE\w*\(\w*\)', 'GETDATE()', content)
+		content = re.sub('\WDATE\w*\(\w*\)', 'GETDATE()', content)
 
 		self.view.replace(edit, region, content)
 
-	def createTrim(sender, matchobj):
+	def createTrim(self, matchobj):
 		return 'rtrim(ltrim(' + matchobj.group(1) + '))'
 
-	def createCaseWhen(sender, matchobj):
+	def createCaseWhen(self, matchobj):
 		return 'CASE WHEN ' + matchobj.group(1) + ' THEN ' + matchobj.group(2) + ' ELSE ' + matchobj.group(3)
